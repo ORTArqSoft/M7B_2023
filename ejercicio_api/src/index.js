f4cc4216ef5dfbafbbb6eb030e2c8fec31fa74d1
Marco Fiorito <marcofiorito1@gmail.com>
2023-03-22 09:49:21 -0300

    ejercicio básico

---
+++ ejercicio_api/src/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const {
+  dwalletBaseUrl,
+  dwalletLoginEndpoint,
+  dwalletRegisterEndpoint,
+  dwalletCashierEndpoint,
+} = require("./utils/endpoints.js");
+const app = express();
+const port = 3000;
+const axios = require("axios");
+
+// New line to support send a body
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+  if (
+    !req.body?.usuario ||
+    !req.body?.password ||
+    !req.body?.idDepartamento ||
+    !req.body?.idCiudad
+  ) {
+    //We will improve this error to have status codes and logging
+    res.send({ error: "You must the user information" });
+  }
+  axios
+    .post(`${dwalletBaseUrl}${dwalletRegisterEndpoint}`, req.body)
+    .then((apiResponse) => {
+      res.send(apiResponse?.data);
+    })
+    .catch((error) => res.send({ error }));
+});
+
+app.post("/login", (req, res) => {
+  if (!req.body?.usuario || !req.body?.password) {
+    res.send({ error: "You must provide a user" });
+  }
+  axios
+    .post(`${dwalletBaseUrl}${dwalletLoginEndpoint}`, {
+      usuario: req.body.usuario,
+      password: req.body.password,
+    })
+    .then((apiResponse) => {
+      res.send(apiResponse?.data);
+    })
+    //We will improve this error to have status codes and logging
+    .catch((error) => res.send({ error }));
+});
+
+app.get("/cashiers", (req, res) => {
+  axios
+    .get(`${dwalletBaseUrl}${dwalletCashierEndpoint}`)
+    .then((apiResponse) => {
+      res.send(apiResponse?.data);
+    })
+    //We will improve this error to have status codes and logging
+    .catch((error) => res.send({ error }));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
---
+++ ejercicio_api/src/utils/endpoints.js
@@ -0,0 +1,14 @@
+const dwalletBaseUrl = "https://dwallet.develotion.com";
+
+const dwalletLoginEndpoint = "/login.php";
+
+const dwalletRegisterEndpoint = "/usuarios.php";
+
+const dwalletCashierEndpoint = "/cajeros.php";
+
+module.exports = {
+  dwalletBaseUrl,
+  dwalletLoginEndpoint,
+  dwalletRegisterEndpoint,
+  dwalletCashierEndpoint,
+};
