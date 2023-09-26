c643ce3f276de0a39c803cf0046ff84b61dd116d
Marco Fiorito <marcofiorito1@gmail.com>
2023-09-26 13:28:35 -0300

    pub/sub example

--- ejemplo_pub_sub/ejemplo_simple/index.js
+++ ejemplo_pub_sub/ejemplo_simple/index.js
@@ -1,38 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const events = require('events');
+const events = require("events");
 
 // Crear un objeto EventEmitter para manejar los eventos
 const eventEmitter = new events.EventEmitter();
 
 // Middleware para analizar el cuerpo de las solicitudes como JSON
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Suscribirse a un evento específico
-app.post('/subscribe/:event', (req, res) => {
+app.post("/subscribe/:event", (req, res) => {
   const event = req.params.event;
 
   // Agregar el cliente actual como suscriptor al evento
   eventEmitter.on(event, (data) => {
+    console.log("me llame");
     res.json(data);
   });
 
-  res.send('Suscripción exitosa');
+  res.send("Suscripción exitosa");
 });
 
 // Publicar un evento
-app.post('/publish/:event', (req, res) => {
+app.post("/publish/:event", async (req, res) => {
   const event = req.params.event;
-  const data = req.body;
 
   // Emitir el evento y pasar los datos
-  eventEmitter.emit(event, data);
+  await eventEmitter.emit(event);
 
-  res.send('Publicación exitosa');
+  res.send("Publicación exitosa");
 });
 
 // Iniciar el servidor
 app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
+  console.log("Servidor iniciado en el puerto 3000");
 });
